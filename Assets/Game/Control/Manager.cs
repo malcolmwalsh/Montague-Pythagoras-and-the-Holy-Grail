0f21a04e22fa31bbe08da33ca652263e3b0c11f8
Mal <malcolmwalsh@gmail.com>
2023-02-15 09:52:51 +1100

    Installed Input System
    Files working, nothing much there yet

---
+++ Assets/Game/Control/Manager.cs
@@ -0,0 +1,40 @@
+﻿using UnityEngine;
+using UnityEngine.InputSystem;
+
+namespace Assets.Game.Control
+{
+    internal class Manager : MonoBehaviour
+    {
+        // Fields
+        private bool exitGame = false;
+        private bool winGame = false;
+
+        //private IPlayer player;
+
+        //private ISet<IRoom> rooms;
+
+        private Key quitKey = Key.Escape;
+        private Key helpKey = Key.H;
+        private Key moveWestKey = Key.W;
+        private Key moveSouthKey = Key.S;
+        private Key moveEastKey = Key.E;
+        private Key moveNorthKey = Key.N;
+
+        private Keyboard keyboard;
+
+        // Methods
+        // Begin MonoBehaviour
+        public void Start()
+        {
+            // Set up all the obstacles
+
+            // Set up all the items
+
+            // Set up all the rooms
+
+            // Hold a reference to the current keyboard
+            keyboard = Keyboard.current;
+
+        }
+    }
+}
